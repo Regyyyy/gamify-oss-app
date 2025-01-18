f67ddc11a5ec6dc8c2caf2b874f463d3a7edeceb
Regyyyy <regyrahman@gmail.com>
2025-01-18 18:33:33 +0700

    setup breeze

--- gamify-oss/resources/views/app.blade.php
+++ gamify-oss/resources/views/app.blade.php
@@ -4,18 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Gamify OSS</title>
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        @routes
         @viteReactRefresh
-        @vite('resources/js/app.jsx')
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <body class="font-sans antialiased">
         @inertia
     </body>
 </html>
