e47eb037c3836248c22c78f83a7a08b5982bde38
Regyyyy <regyrahman@gmail.com>
2025-02-17 22:50:46 +0700

    Update application logo

--- gamify-oss/resources/js/Layouts/GuestLayout.jsx
+++ gamify-oss/resources/js/Layouts/GuestLayout.jsx
@@ -1,14 +1,15 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
+import { Box } from '@mui/material';
 
 export default function GuestLayout({ children }) {
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
+            <Box>
                 <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                    <img src="/images/logo-vertical.png" width="200" height="auto"/>
                 </Link>
-            </div>
+            </Box>
 
             <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 {children}
