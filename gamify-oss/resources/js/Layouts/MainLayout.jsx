28b3c3a348fe262ce9037101805dc1da030637c5
Regyyyy <regyrahman@gmail.com>
2025-05-07 01:41:30 +0700

    fix case sidebar

--- gamify-oss/resources/js/Layouts/MainLayout.jsx
+++ gamify-oss/resources/js/Layouts/MainLayout.jsx
@@ -25,7 +25,7 @@ import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 import Avatar from '@mui/material/Avatar';
-import Sidebar from '@/Components/Sidebar';
+import Sidebar from '@/Components/sidebar';
 import PrimaryButton from '@/Components/PrimaryButton';
 
 export default function MainLayout({ children }) {
